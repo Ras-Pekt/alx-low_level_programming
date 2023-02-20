ad9149838fcffc2360f79ec89e05ba99cf867076
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-02-20 16:09:36 +0300

    print using write()

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: print using write()
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, strlen(str));
+	return (1);
+}
