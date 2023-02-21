deaeb4e931d1e3773332783f28f78f65ad3b33e2
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-02-21 14:08:55 +0300

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main -Entry point
+ * Description: prints _putchar to stdout
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
