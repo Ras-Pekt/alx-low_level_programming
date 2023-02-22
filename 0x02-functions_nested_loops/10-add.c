29cb52591263365881c3003a4500edbab2da7729
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-02-22 18:25:12 +0300

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add 2 numbers
+ * @i: input variable
+ * @j: input variable
+ * Return: Sum
+ */
+
+int add(int i, int j)
+{
+	int sum;
+	
+	sum = i + j;
+	return (sum);
+}
