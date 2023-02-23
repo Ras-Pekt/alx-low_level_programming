b15686cfc81daec84b9b0b8899324f347cfa4daf
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-02-23 16:52:54 +0300

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @i, j, k : input variables
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 1; i <= 100; i++)
+	{
+		j = i % 3;
+		k = i % 5;
+
+		if (j == 0 && k == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (j == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (k == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
