538feb3a3041d4577507c2670ea39b51da8e233a
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-02-23 13:09:33 +0300

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
