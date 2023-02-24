fd02d2f91e78cf7486f1cca382f82b6a6e1a533c
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-02-24 14:30:55 +0300

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @n, @div, @maxFact: input variables
+ * Return: void
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int div = 2, maxFact;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+		{
+			div = div + 1;
+		}
+		else
+		{
+			maxFact = n;
+			n = n / div;
+		}
+		if (n == 1)
+		{
+			printf("%lu is the largest prime factor !\n", maxFact);
+			break;
+		}
+	}
+	return (0);
+}
