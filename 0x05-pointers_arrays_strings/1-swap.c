e3bdbfb936825f4f7194ae96ca5c810dcf1a79d2
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-02-27 15:36:35 +0300

    swap 2 integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps 2 integers
+ * @a: input variable
+ * @b: input variable
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
