1f79e7191caf4e6359bc15a6de61043e809f3aaf
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-02 14:02:59 +0300

    compare 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: input variable
+ * @s2: input variable
+ * Return: 0 if equal, -15 if s1 is shorter, 15 if s2 is shorter
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int s1_counter, s2_counter;
+
+	for (s1_counter = 0; s1[s1_counter] != '\0'; s1_counter++)
+	{
+		for (s2_counter = 0; s2[s2_counter] != '\0'; s2_counter++)
+		{
+		}
+	}
+	if (s1_counter < s2_counter)
+	{
+		return (-15);
+	}
+	else if (s1_counter > s2_counter)
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
+	}
+}
