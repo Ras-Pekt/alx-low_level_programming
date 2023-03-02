44f340ab51b14799e8e74c246361bc2f60b6a766
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-02 14:19:34 +0300

    compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int counter;
 
-	counter = 0;
-	while (s1[counter] != '\0' && s2[counter] != '\0')
+	for (counter = 0; s1[counter] != '\0' && s2[counter] != '\0'; counter++)
 	{
 		if (s1[counter] != s2[counter])
 		{
 			return (s1[counter] - s2[counter]);
 		}
-		counter++;
 	}
 	return (0);
 }
