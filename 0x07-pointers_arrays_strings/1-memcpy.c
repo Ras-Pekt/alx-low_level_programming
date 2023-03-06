369c8410b4c9467741d60527b9da6fb7194f2216
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-06 13:26:42 +0300

    copy n bytes from src to dest

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from src to dest
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: buffer size
+ * Return: pointer to destination memory
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
