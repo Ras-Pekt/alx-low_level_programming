4411700f6f7aebbced6aa2a1e5c9564f67583d35
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-06 14:07:15 +0300

    find char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - finds the 1st occurence of char
+ * @s: input string
+ * @c: char to find
+ * Return: pointer to char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		if (s[count] == c)
+		{
+			return (&s[count]);
+		}
+	}
+	return ('\0');
+}
