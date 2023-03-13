4db41f630a45efe11a10696807dce5c9dff4b1ec
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-13 17:47:31 +0300

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,12 +13,12 @@ char *_strdup(char *str)
 	char *ptr;
 	int i, j;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
 	ptr = malloc(sizeof(char) * i);
-	if (ptr == 0 || str == 0)
-		return (0);
 	for (j = 0; str[j] != '\0'; j++)
 	{
 		ptr[j] = str[j];
