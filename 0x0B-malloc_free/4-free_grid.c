561b9574c7fbed3ee556b1fb7422c97f5113333f
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-14 15:05:05 +0300

    free memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int j;
+
+	for (j = 0; j < height; j++)
+		free(grid[j]);
+	free(grid);
+}
