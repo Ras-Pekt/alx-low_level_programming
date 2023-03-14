42a1aebe2ce0b4a813339560f4c4083cc3208004
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-14 14:09:05 +0300

    create 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
 		{
-			for (i = 0; i; i++)
+			for (i; i = 0; i--)
 				free(ptr[i]);
 			free(ptr);
 			return (0);
