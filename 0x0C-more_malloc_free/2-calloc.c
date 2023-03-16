dc7dbcb70db8d4cf5407602d2e70087ed85be3a0
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-16 16:31:03 +0300

    custom calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *ptr;
+	void *ptr;
 
-	ptr = malloc(nmemb * size);
+	ptr = (char *)malloc(nmemb * size);
 	if (ptr == 0 || nmemb == 0 || size == 0)
 		return (0);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i <= (nmemb * size); i++)
 		ptr[i] = 0;
 	return (ptr);
 }
