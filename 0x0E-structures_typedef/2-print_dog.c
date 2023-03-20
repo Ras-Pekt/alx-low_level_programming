534a24b36c61a57d30bd77908893ad781faf7618
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-20 16:59:51 +0300

    print a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a struct dog
+ * @d: struct d
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		printf("Name: %s\n", d->name);
+		if (d->age == 0)
+			printf("Age: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->name == NULL)
+			printf("Owner: (nil)\n");
+		printf("Owner: %s\n", d->owner);
+	}
+}
