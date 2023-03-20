f1b255e4a1b3c900e4eda8ec958a32768e8b68d7
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-20 17:08:37 +0300

    print a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		printf("Name: %s\n", d->name);
-		if (!(d->age))
+		if (d->age < 0)
 			printf("Age: (nil)\n");
 		printf("Age: %f\n", d->age);
 		if (d->name == NULL)
