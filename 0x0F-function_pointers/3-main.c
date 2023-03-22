e88c8ec0015e1a4b475f7b0cede22c45fd62d3cc
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-22 20:11:23 +0300

    perform simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char **argv)
 {
-	int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
+	int num1, num2;
 	int (*operation)(int, int);
 
 	if (argc != 4)
@@ -25,11 +25,13 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 	operation = get_op_func(argv[2]);
-	if (operation == 0)
+	if (operation == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	printf("%d\n", operation(num1, num2));
 	return (0);
 }
