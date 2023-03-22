c17751427551ece6cebc9309b9ee025a29a4b6b1
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-22 20:06:57 +0300

    perform simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,6 +19,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	operation = get_op_func(argv[2]);
 	if (operation == 0)
 	{
