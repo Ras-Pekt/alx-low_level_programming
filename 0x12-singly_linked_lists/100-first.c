818f3167c33cc9c46739fe9b751f81812d8c7f96
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-30 18:34:11 +0300

    prints before calling main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,7 @@
  * print_before_main - prints before main function call
  * Return: void
  */
+void print_before_main(void) __attribute__((constructor));
 void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
