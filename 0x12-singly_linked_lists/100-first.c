53191d5cbd8f585e817a70788e9fdf0cf7e88aa5
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-03-30 18:25:24 +0300

    prints before calling main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_before_main - prints before main function call
+ * Return: void
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my
+	       back!\n");
+}
