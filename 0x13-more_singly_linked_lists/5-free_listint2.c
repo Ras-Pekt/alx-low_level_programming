875ffd91c5ee6b69a3f5805d93fa45f0bce8f9a0
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-04-03 21:13:30 +0300

    fix free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	head = NULL;
 }
