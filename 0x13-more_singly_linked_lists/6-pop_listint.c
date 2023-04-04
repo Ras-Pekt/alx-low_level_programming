a7703ece001f46dc8797ec3ca32e11e3ed24dada
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-04-04 12:15:19 +0300

    fix delete head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,8 +14,8 @@ int pop_listint(listint_t **head)
 
 	if (head == NULL)
 		return (0);
-	temp = *head;
 	headData = (*head)->n;
+	temp = *head;
 	*head = (*head)->next;
 	free(temp);
 	return (headData);
