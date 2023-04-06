4464de56dd88a4e3546707c30e2e6bb44d206e91
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-04-06 18:23:50 +0300

    get nth bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * get_bit - gets the nth bit
+ * @n: input uint
+ * @index: nth index
+ * Return: value at nth index
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int nth_bit;
+
+	nth_bit = (n >> index) & 1;
+	return (nth_bit);
+}
