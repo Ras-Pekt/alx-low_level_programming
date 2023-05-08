3f27030fcff1f96358477e4806e65ac2f5d9c925
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-05-08 17:27:09 +0300

    fix copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,6 +35,24 @@ void safe_close(int fd, char *b)
 	}
 }
 /**
+ * createBuffer - creates memory of 1024 bytes
+ * @argv: name of file to be written to
+ * Return: memory buffer on success
+ */
+char *createBuffer(char *argv)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * BUFFER);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
  * main - copy file from src to dest
  * @ac: argument count
  * @av: argument array
@@ -46,39 +64,31 @@ int main(int ac, char **av)
 	int fd_file_from, fd_file_to, rd, wr;
 
 	check_argc(ac);
-
-	if ((buff = malloc(sizeof(char) * BUFFER)) == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		free(buff);
-		exit(99);
-	}
+	buff = createBuffer(av[2]);
 
 	fd_file_from = open(av[1], O_RDONLY);
 	rd = read(fd_file_from, buff, BUFFER);
-
-	if (fd_file_from == -1 || rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		free(buff);
-		exit(98);
-	}
-
 	fd_file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	wr = write(fd_file_to, buff, rd);
-
-	if (fd_file_to == -1 || wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		free(buff);
-		exit(99);
-	}
 
 	while (rd > 0)
 	{
-		fd_file_to = open(av[2], O_WRONLY | O_APPEND);
-		rd = read(fd_file_from, buff, BUFFER);
+		if (fd_file_from == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			free(buff);
+			exit(98);
+		}
+
 		wr = write(fd_file_to, buff, rd);
+		if (fd_file_to == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			free(buff);
+			exit(99);
+		}
+
+		rd = read(fd_file_from, buff, BUFFER);
+		fd_file_to = open(av[2], O_WRONLY | O_APPEND);
 	}
 
 	safe_close(fd_file_from, buff);
