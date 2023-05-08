da549abb0dadbd16a3b2e9b9c8e3d9956b993989
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-05-08 18:15:39 +0300

    fix copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "main.h"
-
-#define BUFFER 1024
 
 /**
  * check_argc - checks argument count
@@ -22,36 +19,16 @@ void check_argc(int argc)
 /**
  * safe_close - safely closes file descriptor
  * @fd: file descriptor
- * @b: buffer variable
  * Return: void
  */
-void safe_close(int fd, char *b)
+void safe_close(int fd)
 {
 	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		free(b);
 		exit(100);
 	}
 }
-/**
- * createBuffer - creates memory of 1024 bytes
- * @argv: name of file to be written to
- * Return: memory buffer on success
- */
-char *createBuffer(char *argv)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * BUFFER);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
-		exit(99);
-	}
-	return (buffer);
-}
-
 /**
  * main - copy file from src to dest
  * @ac: argument count
@@ -60,40 +37,38 @@ char *createBuffer(char *argv)
  */
 int main(int ac, char **av)
 {
-	char *buff;
+	char buff[1024];
 	int fd_file_from, fd_file_to, rd, wr;
 
 	check_argc(ac);
-	buff = createBuffer(av[2]);
+
 	fd_file_from = open(av[1], O_RDONLY);
-	rd = read(fd_file_from, buff, BUFFER);
-	fd_file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	while (rd > 0)
+	if (fd_file_from == -1)
 	{
-		fd_file_to = open(av[2], O_WRONLY | O_APPEND);
-		rd = read(fd_file_from, buff, BUFFER);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-		if (fd_file_to == -1 || wr == -1)
+	rd = read(fd_file_from, buff, 1024);
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (rd != -1)
+	{
+		wr = write(fd_file_to, buff, rd);
+		if (fd_file_to == -1 || wr != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			free(buff);
+			safe_close(fd_file_from);
 			exit(99);
-		}
-
-		wr = write(fd_file_to, buff, rd);
-
-		if (fd_file_from == -1 || rd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			free(buff);
-			exit(98);
 		}
 	}
 
-	safe_close(fd_file_from, buff);
-	safe_close(fd_file_to, buff);
-
-	free(buff);
+	safe_close(fd_file_from);
+	safe_close(fd_file_to);
 	exit(EXIT_SUCCESS);
 }
