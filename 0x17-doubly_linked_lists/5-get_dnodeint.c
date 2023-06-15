d3b4b307d14be679783e40793772c5e5cc290697
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-06-15 16:59:59 +0300

    return the sum of all data of a dlistint_t linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
 /**
  * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
  * @head: pointer to head node
- * @index: index og nth node
+ * @index: index of nth node
  * Return: pointer to nth node
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list
+ * @head: pointer to head node
+ * Return: sum
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
