3dec1d3f90f8c4aaeddd0108807931c6f79419c5
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-06 21:31:36 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_table == NULL)
 		return (NULL);
 
+	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 		return (NULL);
 
-	for (; i < size; i++)
+	for (; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
 
 	return (hash_table);
