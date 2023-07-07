7672f2bb382d70694c8203159a82ad698482eb04
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-07 16:20:43 +0300

    add element to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,38 +14,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL)
 		return (0);
 
-	/*malloc'ing memory for new node*/
+	index = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+
+	/*check if key exists to update it*/
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	/*malloc'ing memery for new node*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
-	temp = ht->array[index];
-	if (temp == NULL)
-	{
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
-		new_node->next = NULL;
-		return (1);
-	}
-	else
-	{
-		/*check if key exists to update it*/
-		while (temp)
-		{
-			if (strcmp(temp->key, key) == 0)
-			{
-				free(temp->value);
-				temp->value = strdup(value);
-				return (1);
-			}
-			temp = temp->next;
-		}
-		/*if key does not exist, add it to beginning of list*/
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
-		new_node->next = temp;
-		ht->array[index] = new_node;
-		return (1);
-	}
+	/*if key does not exist, add it to beginning of list*/
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = temp;
+	ht->array[index] = new_node;
+	return (1);
 }
