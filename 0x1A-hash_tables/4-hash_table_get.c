2419d5d7057d659f773e0c935b90779f8851c74e
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-07 17:10:02 +0300

    retrieve value from key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,13 +8,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *temp;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
-		return (NULL);
-	return (ht->array[index]->value);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
 }
