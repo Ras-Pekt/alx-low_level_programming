9b4956c64251df07db8915d53ffec3b9c06b250b
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-07-07 17:59:31 +0300

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *temp;
+	int flag = 0;
+
+	printf("{");
+	for (; index < ht->size; index++)
+	{
+		temp = ht->array[index];
+
+		if (temp == NULL)
+			continue;
+
+		while (temp)
+		{
+			if (flag == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", temp->key, temp->value);
+
+			temp = temp->next;
+		}
+
+		flag = 1;
+	}
+	printf("}\n");
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -5,4 +5,5 @@ This directory contains exercises on creating and using hash tables in C. They i
 - [2-key_index.c](2-key_index.c) - gives you the index of a key
 - [3-hash_table_set.c](3-hash_table_set.c) - adds an element to the hash table
 - [4-hash_table_get.c](4-hash_table_get.c) - retrieves a value associated with a key
--
+- [5-hash_table_print.c](5-hash_table_print.c) - prints a hash table
+- 
