a8f7b51f9f284e2f415df13d5d4abed471bd0876
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-08-23 18:14:37 +0300

    calculate perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,15 +8,17 @@ def island_perimeter(grid):
     """
     returns the perimeter of the island described in grid
     """
-    i = 0
-    for lst in grid:
-        print(f"{i}: ", end="")
-        print(lst)
-        i += 1
-        for li in lst:
-            if li == 1:
-                print(f"Index:  {lst.index(li)}")
-                idx = lst.index(li)
-                if lst[idx + 1] == 1:
-                    print("Another 1")
-            #print (f"Inside loop: {li}")
+    rows =  len(grid)
+    columns = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
